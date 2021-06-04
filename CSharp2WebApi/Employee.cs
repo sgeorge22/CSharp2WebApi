aa61929de4a4508b5b56fc0a6d55cfe354fbb262
Sarah George <sarahgeorge1394@gmail.com>
2021-06-04 14:55:15 -0400

    Add project files.

---
+++ CSharp2WebApi/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CSharp2WebApi
+{
+    class Employee //We copied this from the employee class from the PoWebApi project nd brought over
+    {
+        public int Id { get; set; } 
+        [Required, StringLength(30)]
+        public string Login { get; set; }
+        [Required, StringLength(30)]
+        public string Password { get; set; }
+        [Required, StringLength(30)]
+        public string Firstname { get; set; }
+        [Required, StringLength(30)]
+        public string Lastname { get; set; }
+        [Required]
+        public bool IsManager { get; set; } 
+
+        public Employee() { }
+    }
+}
---
+++ CSharp2WebApi/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CSharp2WebApi
+{
+    class Program
+    {
+        async Task Run() //doesnt have the word static so is an instance method and can access almost everything
+        {
+            var http = new HttpClient();
+            var jsonSerializerOptions = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+
+            };
+            var url = "http://localhost:4702/api/employees";
+
+            var newEmpl = new Employee()
+            {
+                Id = 0,
+                Firstname = "Noah",
+                Lastname = "Phence",
+                Login = "nphence",
+                Password = "password",
+                IsManager = true
+            };
+            var json = JsonSerializer.Serialize<Employee>(newEmpl, jsonSerializerOptions);
+            var httpContent2 = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var httpMessageResponse2 = await http.PostAsync(url, httpContent2);
+
+            var httpMessageResponse = await http.GetAsync(url);
+            var httpContent = await httpMessageResponse.Content.ReadAsStringAsync();
+            var employees = JsonSerializer.Deserialize<Employee[]>(httpContent, jsonSerializerOptions);
+
+            foreach(var e in employees)
+            {
+                Console.WriteLine($"{e.Id} | {e.Lastname}");
+            }
+            
+        }
+
+        async static Task Main(string[] args)
+        {
+            var pgm = new Program();
+            await pgm.Run();
+        }
+    }
+}
